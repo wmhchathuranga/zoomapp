be31ee574ba77395cc2fd8f0c8249aea1a647078
chathuranga <harshana.all@gmail.com>
2023-11-26 06:22:43 +0530

    db update

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const crypto = require('crypto')
 // mongodb connection
-const {MongoClient} = require('mongodb');
+const {MongoClient, ServerApiVersion} = require('mongodb');
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-client.connect().then((err, client) => {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-        return;
-    }
-    console.log('Connected to MongoDB');
 
-})
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ping: 1});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 const app = express()
 const port = process.env.PORT || 4000
